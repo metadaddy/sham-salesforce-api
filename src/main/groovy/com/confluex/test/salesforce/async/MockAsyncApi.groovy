4278a1211ec06eb5f985d66145fd26f3770f412a
Pat Patterson <metadaddy@gmail.com>
2016-09-13 13:18:37 -0700

    Filled out MockAsyncApi. Accept records from test code, return then in bulk query response.

--- src/main/groovy/com/confluex/test/salesforce/async/MockAsyncApi.groovy
+++ src/main/groovy/com/confluex/test/salesforce/async/MockAsyncApi.groovy
@@ -11,6 +11,8 @@ import static com.confluex.mock.http.matchers.HttpMatchers.path
 
 class MockAsyncApi {
     MockHttpsServer mockHttpsServer
+    String version
+    LinkedHashMap<String, LinkedHashMap<String, String>> records = new LinkedHashMap<>()
 
     MockAsyncApi(MockHttpsServer mockHttpsServer) {
         this.mockHttpsServer = mockHttpsServer
@@ -18,9 +20,51 @@ class MockAsyncApi {
     }
 
     void defaults() {
-        batchResult().respondSuccess()
+        // Do everything here for now
+        //batchResult().respondSuccess()
 
-        def batchPathPattern = '/services/async/26\\.0/job/(\\w+)/batch'
+        // Create job
+        // https://developer.salesforce.com/docs/atlas.en-us.api_asynch.meta/api_asynch/asynch_api_jobs_create.htm
+        def jobPathPattern = '/services/async/(\\d+.\\d)/job/'
+        mockHttpsServer.respondTo(path(matchesPattern(jobPathPattern))).withStatus(201).withBody { request ->
+            def requestXml = new XmlSlurper().parseText(request.body)
+            def version = (request.path =~ jobPathPattern)[0][1]
+            slurpAndEditXml('/template/create-job-response.xml') { root ->
+                def operation = requestXml.operation.text()
+                // return "00MOCK0000query" etc so we know what kind of batch result to return
+                root.id = '00MOCK' + operation.padLeft(9, '0')
+                root.operation = operation
+                root.object = requestXml.object.text()
+                root.externalIdFieldName = requestXml.externalIdFieldName.text()
+                root.createdDate = formatDate(new Date())
+                root.systemModstamp = formatDate(new Date())
+                root.apiVersion = version
+            }
+        }
+
+        // Monitor/change job state - e.g. abort/close job
+        // https://developer.salesforce.com/docs/atlas.en-us.api_asynch.meta/api_asynch/asynch_api_jobs_close.htm
+        // https://developer.salesforce.com/docs/atlas.en-us.api_asynch.meta/api_asynch/asynch_api_jobs_get_details.htm
+        // https://developer.salesforce.com/docs/atlas.en-us.api_asynch.meta/api_asynch/asynch_api_jobs_abort.htm
+        def jobInfoPathPattern = '/services/async/(\\d+.\\d)/job/(\\w+)'
+        mockHttpsServer.respondTo(path(matchesPattern(jobInfoPathPattern))).withStatus(200).withBody { request ->
+            def requestXml = new XmlSlurper().parseText(request.body)
+            def version = (request.path =~ jobInfoPathPattern)[0][1]
+            def jobId = (request.path =~ jobInfoPathPattern)[0][2]
+            slurpAndEditXml('/template/create-job-response.xml') { root ->
+                root.id = jobId
+                root.operation = 'query'
+                root.object = 'Account'
+                root.state = requestXml.state.text()
+                root.createdDate = formatDate(new Date())
+                root.systemModstamp = formatDate(new Date())
+                root.apiVersion = version
+            }
+        }
+
+        // Add a batch to a job
+        // https://developer.salesforce.com/docs/atlas.en-us.api_asynch.meta/api_asynch/asynch_api_batches_create.htm
+        def batchPathPattern = '/services/async/\\d+.\\d/job/(\\w+)/batch'
         mockHttpsServer.respondTo(path(matchesPattern(batchPathPattern))).withStatus(201).withBody { request ->
             def jobId = (request.path =~ batchPathPattern)[0][1]
             slurpAndEditXml('/template/create-batch-response.xml') { root ->
@@ -30,16 +74,54 @@ class MockAsyncApi {
                 root.systemModstamp = formatDate(new Date())
             }
         }
-        mockHttpsServer.respondTo(path('/services/async/26.0/job/')).withStatus(201).withBody { request ->
-            def requestXml = new XmlSlurper().parseText(request.body)
-            slurpAndEditXml('/template/create-job-response.xml') { root ->
-                root.id = '00MOCK000000job'
-                root.operation = requestXml.operation.text()
-                root.object = requestXml.object.text()
-                root.externalIdFieldName = requestXml.externalIdFieldName.text()
+
+        // Get batch info
+        // https://developer.salesforce.com/docs/atlas.en-us.api_asynch.meta/api_asynch/asynch_api_batches_get_info.htm
+        def batchInfoPathPattern = '/services/async/\\d+.\\d/job/(\\w+)/batch/(\\w+)'
+        mockHttpsServer.respondTo(path(matchesPattern(batchInfoPathPattern))).withStatus(200).withBody { request ->
+            def matches = (request.path =~ batchInfoPathPattern)
+            def jobId = matches[0][1]
+            def batchId = matches[0][2]
+            slurpAndEditXml('/template/batch-info-response.xml') { root ->
+                root.id = batchId
+                root.jobId = jobId
                 root.createdDate = formatDate(new Date())
                 root.systemModstamp = formatDate(new Date())
             }
+        }
+
+        // Get batch results
+        // https://developer.salesforce.com/docs/atlas.en-us.api_asynch.meta/api_asynch/asynch_api_using_bulk_query.htm
+        def batchResultPathPattern = '/services/async/\\d+.\\d/job/(\\w+)/batch/(\\w+)/result'
+        mockHttpsServer.respondTo(path(matchesPattern(batchResultPathPattern))).withStatus(200).withBody { request ->
+            def matches = (request.path =~ batchResultPathPattern)
+            def jobId = matches[0][1]
+            def batchId = matches[0][2]
+            def response
+            if (jobId.endsWith('query')) {
+                response = slurpAndEditXml('/template/batch-result-list-response.xml') { root ->
+                    root.result = '00MOCK000result'
+                }
+            } else {
+                // Default copied from MockBatchResultBuilder
+                response = slurpAndEditXml('/template/batch-result-response.xml') { root ->
+                    root.result.id = batchId
+                    root.result.success = 'true'
+                }
+            }
+        }
+
+        // Get bulk query result
+        // https://developer.salesforce.com/docs/atlas.en-us.api_asynch.meta/api_asynch/asynch_api_using_bulk_query.htm
+        def bulkQueryResultsPathPattern = '/services/async/\\d+.\\d/job/\\w+/batch/\\w+/result/\\w+'
+        mockHttpsServer.respondTo(path(matchesPattern(bulkQueryResultsPathPattern)))
+            .withStatus(200)
+            .withHeader("Content-Type", "text/csv").withBody {
+            String content = "\"Id\",\"Name\"\n"
+            records.each { id, fields ->
+                content += "\"" + id + "\",\"" + fields.get("Name") + "\"\n"
+            }
+            content
         }
     }
 
@@ -65,4 +147,11 @@ class MockAsyncApi {
         date.format("yyyy-MM-dd'T'HH:mm:ss'Z'")
     }
 
+    void clear() {
+        this.records.clear()
+    }
+
+    void insert(LinkedHashMap<String, LinkedHashMap<String, String>> records) {
+        this.records.putAll(records)
+    }
 }
